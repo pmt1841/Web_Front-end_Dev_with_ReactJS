a0a7e0d3f82779140cc0fc0549689b4c1310419a
pmt1841 <phamminhtien1841@gmail.com>
2026-04-17 16:53:50 +0700

    push

---
+++ 2.ReactJS_JSX/student-info-table/src/main.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const students = [
+    {
+        company: 'Alfreds Futterkiste',
+        contact: 'Maria Anders',
+        country: 'Germany'
+    },
+    {
+        company: 'Centro comercial Moctezuma',
+        contact: 'Francisco Chang',
+        country: 'Mexico'
+    },
+    {
+        company: 'Ernst Handel',
+        contact: 'Roland Mendel',
+        country: 'Austria'
+    },
+    {
+        company: 'Island Trading',
+        contact: 'Helen Bennett',
+        country: 'UK'
+    },
+    {
+        company: 'Laughing Bacchus Winecellars',
+        contact: 'Yoshi Tannamuri',
+        country: 'Canada'
+    },
+    {
+        company: 'Magazzini Alimentari Riuniti',
+        contact: 'Giovanni Rovelli',
+        country: 'Italy'
+    }
+]
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const element = (
+    <div>
+        <h2 style={{textAlign : "center"}}> Students </h2>
+        <table>
+            <tbody>
+            <tr>
+                <th>Company</th>
+                <th>Contact</th>
+                <th>Country</th>
+            </tr>
+            {students.map(student => (
+                <tr>
+                    <td>{student.company}</td>
+                    <td>{student.contact}</td>
+                    <td>{student.country}</td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    </div>
+);
+root.render(element);
