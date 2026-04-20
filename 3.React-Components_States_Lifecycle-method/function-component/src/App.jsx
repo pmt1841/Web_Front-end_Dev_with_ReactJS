66e3cb94fc3459df96f17aedcde952aa33f87c06
pmt1841 <phamminhtien1841@gmail.com>
2026-04-20 11:47:34 +0700

    push

---
+++ 3.React-Components_States_Lifecycle-method/function-component/src/App.jsx
@@ -0,0 +1,11 @@
+import AddComponent from './components/AddComponent.jsx';
+
+function App() {
+    return (
+        <div className="App">
+            <AddComponent firstNumber={1} secondNumber={2}/>
+        </div>
+    );
+}
+
+export default App;
