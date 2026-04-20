abae34895c0adbef3c024d74d0e0fa1d2bb86391
pmt1841 <phamminhtien1841@gmail.com>
2026-04-20 11:58:29 +0700

    push

---
+++ 3.React-Components_States_Lifecycle-method/reactjs-count-func/src/App.jsx
@@ -0,0 +1,23 @@
+import {useState} from 'react';
+
+function App() {
+    const [number, setNumber] = useState(0);
+
+    const increase = () => {
+        setNumber(number + 1);
+    }
+
+    const decrease = () => {
+        setNumber(number - 1);
+    }
+
+    return (
+        <div style={{textAlign: "center", padding: 30}}>
+            <button onClick={decrease}>Decrease</button>
+            <span style={{padding: 10}}>{number}</span>
+            <button onClick={increase}>Increase</button>
+        </div>
+    );
+}
+
+export default App;
