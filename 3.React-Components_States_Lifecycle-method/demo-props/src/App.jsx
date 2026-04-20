fbf0fd6530dce2b9025af65d8cc7272fa1ab72e8
pmt1841 <phamminhtien1841@gmail.com>
2026-04-20 11:38:18 +0700

    push

---
+++ 3.React-Components_States_Lifecycle-method/demo-props/src/App.jsx
@@ -0,0 +1,11 @@
+import Welcome from './components/Welcome';
+
+function App() {
+  return (
+      <div className="App">
+        <Welcome name="Admin" />
+      </div>
+  );
+}
+
+export default App;
---
+++ 3.React-Components_States_Lifecycle-method/demo-props/src/components/welcome.jsx
@@ -0,0 +1,6 @@
+function Welcome(props) {
+    console.log(props);
+    return <h1>Hello: {props.name}</h1>;
+}
+
+export default Welcome;
