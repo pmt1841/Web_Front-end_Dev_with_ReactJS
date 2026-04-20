a39d2a5e23ccf0def15eb1f9d46c429b40c7052d
pmt1841 <phamminhtien1841@gmail.com>
2026-04-20 15:24:54 +0700

    push

---
+++ 3.React-Components_States_Lifecycle-method/reactjs-alert-component/src/App.jsx
@@ -0,0 +1,12 @@
+import Alert from "./components/Alert";
+
+function App() {
+    return (
+        <div>
+            <h1>Hệ thống thông báo</h1>
+            <Alert text = "Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại." />
+        </div>
+    );
+}
+
+export default App;
---
+++ 3.React-Components_States_Lifecycle-method/reactjs-alert-component/src/components/Alert.jsx
@@ -0,0 +1,9 @@
+function Alert(props) {
+    return (
+        <div className="alert alert-warning" role="alert">
+            {props.text}
+        </div>
+    );
+}
+
+export default Alert;
