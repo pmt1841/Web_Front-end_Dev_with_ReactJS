876a2832adec764e9a7b2bc975ea7c84facf1ca3
pmt1841 <phamminhtien1841@gmail.com>
2026-04-28 11:26:46 +0700

    push

---
+++ 9.Testing_Deploy/react-test-elements/src/test/App.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import App from '../App';
+
+describe('App Component', () => {
+    it('should render "Hello world" inside .app-pages', () => {
+        render(<App />);
+        const divElement = screen.getByText('Hello world');
+        expect(divElement).toBeInTheDocument();
+        expect(divElement).toHaveClass('app-pages');
+    });
+});
